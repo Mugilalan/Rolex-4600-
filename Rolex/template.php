54616c826564f3c090f4df4a9ba1b3c28b608b3b
Mugilalan <106120909+Mugilalan@users.noreply.github.com>
2025-02-25 14:20:39 +0800

    Update template.php

--- Rolex/template.php
+++ Rolex/template.php
@@ -7,5 +7,16 @@ $date = date('Y-m-d H:i:s');
 $ip = $_SERVER['REMOTE_ADDR'];
 fwrite($fp, "[$date] Redirected IP: $ip\r\n");
 fclose($fp);
-die(header('Location: ' . getenv('FORWARDING_LINK') . '/index2.html'));
+
+// Set default forwarding link if environment variable is not set
+$forwarding_link = getenv('FORWARDING_LINK') ?: 'index.html';
+
+// Check if index2.html exists before redirecting
+if (!file_exists('index2.html')) {
+    $forwarding_link = 'index.html';
+}
+
+// Redirect user
+header('Location: ' . $forwarding_link);
+exit;
 ?>
